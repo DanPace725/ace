bb69dc87179ebf8583145b7d61e6b26982986f15
DanPace725 <118482982+DanPace725@users.noreply.github.com>
2024-09-23 17:12:19 -0700

    edit supabasecliient

--- lib/supabaseClient.tsx
+++ lib/supabaseClient.tsx
@@ -1,4 +1,4 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/types/supabase/supabase' // Make sure you have this type defined
+//import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+//import { Database } from '@/types/supabase/supabase' // Make sure you have this type defined
 
 //export const supabase = createClientComponentClient<Database>()
